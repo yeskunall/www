f09477bb071f3fd10d87620504ca38f9a63e73e3
Kunall Banerjee <hey@kimchiii.space>
2023-10-19 15:50:51 -0400

    feat: refresh blog layout

--- src/lib/rehype-plugins/pretty-code/options.ts
+++ src/lib/rehype-plugins/pretty-code/options.ts
@@ -1,12 +1,6 @@
-import { readFileSync } from "node:fs";
-
 import type { Options } from "rehype-pretty-code";
 
 export const rehypePrettyCodeOptions: Partial<Options> = {
   grid: true,
-  theme: JSON.parse(
-    readFileSync(
-      new URL("./themes/one-hunter-vercel.json", import.meta.url),
-    ).toString(),
-  ),
+  theme: "rose-pine-dawn",
 };
