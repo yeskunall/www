65d4664b623f7601171b4dcfb88ffd63d686b454
Kunall Banerjee <hey@kimchiii.space>
2023-10-19 15:28:27 -0400

    chore: move projects data

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,19 +1,24 @@
-export const MENU_LINKS = [
+/* eslint-disable max-len */
+
+export const PROJECTS = [
   {
-    title: "About",
-    path: "/about/",
+    name: "Raycast extension for Anilist",
+    description: "Get the airing schedule of anime from Anilist.co",
+    href: "https://www.raycast.com/yeskunall/anilist-airing-schedule",
+    logo: "https://logo.clearbit.com/raycast.com",
   },
   {
-    title: "Blog",
-    path: "/writing/",
+    name: "Deep Rainforest",
+    description:
+      "A VS Code theme with lush green hues & serene blue undertones",
+    href: "https://marketplace.visualstudio.com/items?itemName=yeskunall.deep-rainforest",
+    logo: "https://logo.clearbit.com/marketplace.visualstudio.com",
   },
   {
-    title: "Projects",
-    path: "/projects/",
-  },
-  {
-    title: "Now",
-    path: "/now/",
+    name: "HN Digest",
+    description: "Weekly digest of top stories from Hacker News",
+    href: "",
+    logo: "https://logo.clearbit.com/news.ycombinator.com",
   },
 ];
 
