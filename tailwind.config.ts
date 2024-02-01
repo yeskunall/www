7074ed3d19fe0eb2eb087309700f8253cd65601e
Kunall Banerjee <hey@kimchiii.space>
2024-02-01 14:48:54 -0500

    style: update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,99 +1,203 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
 
 import type { Config } from "tailwindcss";
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
+  corePlugins: {
+    aspectRatio: false,
+    container: false,
+    fontVariantNumeric: false,
+    lineClamp: false,
+    touchAction: false,
+  },
   theme: {
     extend: {
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "eye-movement": "eye-movement 12s infinite ease",
-        "rotate-head": "rotate-head 12s infinite ease",
-        "slide-down": "slide-down 210ms cubic-bezier(0.3, 0, 0.5, 1)",
-        "slide-up": "slide-up 210ms cubic-bezier(0.3, 0, 0.5, 1)",
+      colors: {
+        gray: generateScale("mauve"),
+        ruby: generateScale("ruby"),
       },
       fontFamily: {
-        mono: ["Martian Mono", ...fontFamily.mono],
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-        serif: ["Newsreader", ...fontFamily.serif],
-        signature: ["Brittany Signature"],
+        sans: ["var(--font-satoshi)", ...fontFamily.sans],
+        serif: ["var(--font-newsreader)", ...fontFamily.serif],
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-        "eye-movement": {
-          "0%": {
-            transform: "translateX(0px) translateY(0) scaleY(1)",
-          },
-          "20%": {
-            transform: "translateX(2px) translateY(0) scaleY(1)",
-          },
-          "30%": {
-            transform: "translateX(-2px) translateY(0) scaleY(1)",
-          },
-          "55%": {
-            transform: "translateX(0px) translateY(4px) scaleY(1)",
-          },
-          "65%": {
-            transform: "translateX(0px) translateY(0px) scaleY(0.5)",
-          },
-          "70%": {
-            transform: "translateX(0px) translateY(0px) scaleY(1)",
-          },
-          "100%": {
-            transform: "translateX(0px) translateY(0px) scaleY(1)",
-          },
-        },
-        "rotate-head": {
-          "0%": {
-            "box-shadow": "inset 2px -2px 3px var(--text-secondary)",
-          },
-          "20%": {
-            "box-shadow": "inset 4px -2px 3px var(--text-secondary)",
-          },
-          "30%": {
-            "box-shadow": "inset 2px -2px 3px var(--text-secondary)",
-          },
-          "55%": {
-            "box-shadow": "inset 4px 1px 3px var(--text-secondary)",
-          },
-          "65%": {
-            "box-shadow": "inset 2px -2px 3px var(--text-secondary)",
-          },
-          "100%": {
-            "box-shadow": "inset 2px -2px 3px var(--text-secondary)",
-          },
-        },
-        "slide-down": {
-          from: {
-            height: "0",
-            opacity: "0",
-          },
-          to: {
-            height: "var(--radix-collapsible-content-height)",
-            opacity: "1",
-          },
-        },
-        "slide-up": {
-          from: {
-            height: "var(--radix-collapsible-content-height)",
-            opacity: "1",
-          },
-          to: {
-            height: "0",
-            opacity: "0",
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: remove once Tailwind exposes the type
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "var(--mauve-11)",
+            "--tw-prose-bold": "var(--mauve-11)",
+            "--tw-prose-code": "var(--mauve-11)",
+            "--tw-prose-headings": "var(--mauve-11)",
+            "--tw-prose-links": "var(--mauve-11)",
+            ":where(h1, h2, h3, h4, h5, h6):not(:where([class~='not-prose'], [class~='not-prose'] *))":
+              {
+                "--type-heading":
+                  "italic 600 21px / 1.2 var(--font-eb-garamond)",
+                font: "var(--type-heading)",
+                letterSpacing: "-0.05em",
+                "> a": {
+                  textDecoration: "none",
+                },
+              },
+            blockquote: {
+              borderLeftWidth: "0",
+              paddingRight: theme("spacing.5"),
+              position: "relative",
+              quotes: "none",
+              "&::before": {
+                backgroundImage:
+                  "linear-gradient(30deg,#191919,#3f3f3f,#e1e1e1)",
+                boxShadow: "1px 10px 5px 0 #e1e1e1",
+                content: "''",
+                height: "calc(100% + 1px)",
+                left: "0px",
+                position: "absolute",
+                top: "0px",
+                width: "1px",
+              },
+              "& > p": {
+                font: "var(--type-ui)",
+                color: "var(--mauve-a11)",
+                "& > a": {
+                  textDecoration: "none",
+                },
+              },
+            },
+            li: {
+              lineHeight: "1.225",
+            },
+            p: {
+              font: "normal 420 var(--font-size) / var(--line-height) var(--font-newsreader)",
+              letterSpacing: "-0.05em",
+            },
+            "p:has(img)": {
+              "@apply wide-wrapper": {},
+              "@media (min-width: 640px)": {
+                "--wrap-wide": "65vw",
+              },
+              "> em": {
+                "@apply mx-3 block text-center font-sans text-[16px] font-medium not-italic sm:mx-0":
+                  {},
+              },
+              "> img": {
+                "@apply sm:rounded-md": {},
+                aspectRatio: "16 / 9",
+                marginBottom: theme("spacing.3"),
+                objectFit: "cover",
+                width: "100%",
+              },
+            },
           },
         },
-      },
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    plugin(function addBaseStyles({ addBase }) {
+      addBase({
+        ":where(em, i):not(:where([class~='prose'], [class~='prose'] *))": {
+          "--type-italic":
+            "normal italic 390 calc(var(--font-size) + 2px) / 1.225 var(--font-newsreader)",
+          font: "var(--type-italic)",
+        },
+        ":where(h1, h2, h3, h4, h5, h6)": {
+          textWrap: "balance",
+        },
+        ":where(p):not(:where([class~='not-prose'], [class~='not-prose'] *))": {
+          font: "normal 420 calc(var(--font-size) + 2px) / var(--line-height) var(--font-newsreader)",
+          letterSpacing: "-0.05em",
+        },
+        "b, strong": {
+          fontWeight: "400",
+        },
+        body: {
+          color: "var(--mauve-11)",
+          fontSynthesis: "none",
+          textWrap: "pretty",
+        },
+        html: {
+          "@apply selection:bg-ruby-3 selection:text-ruby-11": {},
+          "-webkit-font-smoothing": "antialiased",
+          "-moz-osx-font-smoothing": "grayscale",
+          backgroundColor: "var(--bg)",
+          fontSize: "16px",
+          scrollBehavior: "smooth",
+          touchAction: "manipulation",
+        },
+      });
+    }),
+    plugin(function addFontEBGaramond({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "EB Garamond",
+          src: `url("/fonts/eb-garamond-subset-italic.woff2") format("woff2")`,
+          fontDisplay: "swap",
+          fontStyle: "italic",
+          fontWeight: "600",
+        },
+      });
+    }),
+    plugin(function addFontNewsreader({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Newsreader",
+          src: `url("/fonts/newsreader.woff2") format("woff2")`,
+          fontDisplay: "swap",
+          fontStyle: "normal",
+          fontWeight: "200 800",
+        },
+      });
+    }),
+    plugin(function addFontNewsreaderItalic({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Newsreader",
+          src: `url("/fonts/newsreader-italic.woff2") format("woff2")`,
+          fontDisplay: "swap",
+          fontStyle: "italic",
+          fontWeight: "200 800",
+        },
+      });
+    }),
+    plugin(function addFontSatoshi({ addBase }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "Satoshi",
+          src: `url("/fonts/satoshi.woff2") format("woff2")`,
+          fontDisplay: "swap",
+          fontStyle: "normal",
+          fontWeight: "300 900",
+        },
+      });
+    }),
+    plugin(function wideWrapperPlugin({ addComponents }) {
+      addComponents({
+        ".wide-wrapper": {
+          maxWidth: "min(100vw, var(--wrap-wide))",
+          width: "min(100vw, var(--wrap-wide))",
+          marginLeft: "calc((min(100vw, var(--wrap-wide)) - 100%) / -2)",
+          "--wrap-wide": "100vw",
+        },
+      });
+    }),
+  ],
 } satisfies Config;
+
+function generateScale(name: string) {
+  const scale = Array.from({ length: 12 }, (_, i) => {
+    const id = i + 1;
+
+    return [
+      [id, `var(--${name}-${id})`],
+      [`a${id}`, `var(--${name}-a${id})`],
+    ];
+  }).flat();
+
+  return Object.fromEntries(scale);
+}
