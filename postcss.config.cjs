aacdab81873df13c240481b3afc1ff4fcb9e89fb
Kunall Banerjee <hey@kimchiii.space>
2024-04-03 03:12:59 -0400

    docs: add note

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
+    // Extraneous step: this might be unneeded because I set `nesting` in `@astro/tailwind` as well
     require("tailwindcss/nesting"),
     require("tailwindcss"),
     require("autoprefixer"),
