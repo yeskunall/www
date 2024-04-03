af1e75e3ab1e4a33a57b9da9556f523f1a1c75e5
Kunall Banerjee <hey@kimchiii.space>
2024-04-03 03:17:50 -0400

    refactor: update content collections config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,21 @@
 import { defineCollection, z } from "astro:content";
 
-function removeDupsAndLowerCase(tags: string[]) {
-  if (!tags.length) return tags;
-
-  const lowercaseItems = tags.map(tag => tag.toLowerCase());
-  const distinctItems = new Set(lowercaseItems);
-
-  return Array.from(distinctItems);
-}
+import { removeDupsAndLowerCase } from "~/lib/post";
 
 const post = defineCollection({
-  type: "content",
   schema: z.object({
-    title: z.string().max(60),
-    description: z.string().max(160).or(z.string().max(0)),
-    featured: z.boolean().optional(),
+    description: z.string().max(360).or(z.string().max(0)).optional(),
+    lastUpdatedDate: z
+      .string()
+      .transform(string => new Date(string))
+      .optional(),
     publishDate: z.string().transform(string => new Date(string)),
     tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+    title: z.string().max(60),
   }),
+  type: "content",
 });
 
-export const collections = { post };
+export const collections = {
+  post,
+};
