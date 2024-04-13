33733c6bbc012c997793ccfd5fbfe39af7560388
Kunall Banerjee <hey@kimchiii.space>
2024-04-13 10:13:59 -0400

    feat: 🎸 show cover image (optional)

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,16 +3,23 @@ import { defineCollection, z } from "astro:content";
 import { removeDupsAndLowerCase } from "~/lib/post";
 
 const post = defineCollection({
-  schema: z.object({
-    description: z.string().max(360).or(z.string().max(0)).optional(),
-    lastUpdatedDate: z
-      .string()
-      .transform(string => new Date(string))
-      .optional(),
-    publishDate: z.string().transform(string => new Date(string)),
-    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-    title: z.string().max(60),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      coverImage: z
+        .object({
+          alt: z.string(),
+          src: image(),
+        })
+        .optional(),
+      description: z.string().max(360).or(z.string().max(0)).optional(),
+      lastUpdatedDate: z
+        .string()
+        .transform(string => new Date(string))
+        .optional(),
+      publishDate: z.string().transform(string => new Date(string)),
+      tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+      title: z.string().max(60),
+    }),
   type: "content",
 });
 
