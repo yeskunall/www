6d4e3ae3d11dbc9dc10aa9accd0ad7d616155a03
Kunall Banerjee <hey@kimchiii.space>
2024-05-29 23:37:22 -0400

    build(site): update meta

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
   author: "Kunall Banerjee",
-  description:
-    "Digital craftsman, building modest software & websites on the internet",
+  description: "Digital craftsman, building modest products on the internet",
   ogImage: "default-og.png",
   title: "Kunall Banerjee — I create",
   url: "https://kimchiii.space",
