e3306b24bfe44084772aeb5b5f7bd4024b232f93
Kunall Banerjee <hey@kimchiii.space>
2024-06-04 17:47:28 -0400

    build(postcss): simplify PostCSS config
    Here’s hoping that the next update will drop this file altogether. The “next update” being the additon of Lightning CSS and Tailwind CSS 4. Can’t wait.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    // Extraneous step: this might be unneeded because I set `nesting` in `@astro/tailwind` as well
-    require("tailwindcss/nesting"),
     require("tailwindcss"),
-    require("autoprefixer"),
     ...(process.env.NODE_ENV === "production"
       ? // eslint-disable-next-line @typescript-eslint/no-var-requires
         [require("cssnano")({ preset: "advanced" })]
