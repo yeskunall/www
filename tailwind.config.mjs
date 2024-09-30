0b7adcb24d67d71847f07f29e997020f046d2e7c
Kunall Banerjee <hey@kimchiii.space>
2024-09-30 09:04:38 -0400

    fix(tailwindcss): add missing plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,6 +20,7 @@ export default {
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
+    require("tailwindcss-animate"),
     plugin(({ addComponents }) => {
       addComponents({
         ".wide-wrapper": {
