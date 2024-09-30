d4f82c2ea17622a8c28ab8d543df62b6457f6683
Kunall Banerjee <hey@kimchiii.space>
2024-09-30 06:35:25 -0400

    fix(umami): fix page tracking in Firefox

--- src/scripts/vendor/umami.ts
+++ src/scripts/vendor/umami.ts
@@ -2,10 +2,7 @@ if (import.meta.env.DEV) {
   localStorage.setItem("umami.disabled", "1");
 }
 
-if ("startViewTransition" in document) {
-  document.addEventListener("astro:page-load", () => {
-    // Run after every additional navigation by Astro
-
-    void window.umami.track(props => ({ ...props }));
-  });
-}
+document.addEventListener("astro:page-load", () => {
+  // Run after every additional navigation by Astro
+  void window.umami.track(props => ({ ...props }));
+});
